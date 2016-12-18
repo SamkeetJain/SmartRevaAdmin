1ba8231ecc248f956e809c4cf3f8d79be8b2c227
Samkeet Jain <jain.sankeet2210@gmai.com>
2016-12-18 09:28:30 +0530

    UI

--- app/src/main/java/com/samkeet/smartrevaadmin/MainActivity.java
+++ app/src/main/java/com/samkeet/smartrevaadmin/MainActivity.java
@@ -1,7 +1,14 @@
 package com.samkeet.smartrevaadmin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.samkeet.smartrevaadmin.alumni.AlumniMainActivity;
+import com.samkeet.smartrevaadmin.councling.CounclingMain;
+import com.samkeet.smartrevaadmin.fees.FeesMain;
+import com.samkeet.smartrevaadmin.placements.PlacementsMain;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Placements(View v) {
+        Intent intent = new Intent(getApplicationContext(), PlacementsMain.class);
+        startActivity(intent);
+    }
+
+    public void Councling(View v) {
+        Intent intent = new Intent(getApplicationContext(), CounclingMain.class);
+        startActivity(intent);
+    }
+
+    public void Alumni(View v){
+        Intent intent = new Intent(getApplicationContext(),AlumniMainActivity.class);
+        startActivity(intent);
+    }
+
+    public void Fees (View v){
+        Intent intent = new Intent(getApplicationContext(), FeesMain.class);
+        startActivity(intent);
     }
 }
