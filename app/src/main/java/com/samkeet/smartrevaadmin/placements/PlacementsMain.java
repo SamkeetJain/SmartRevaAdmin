8bf55e83acd48712f42eb04ecb379586cf93d8d0
vaibhavkrishna.bhosle@gmail.com <kingline>
2017-01-10 01:36:31 +0530

    placement stage 1

--- app/src/main/java/com/samkeet/smartrevaadmin/placements/PlacementsMain.java
+++ app/src/main/java/com/samkeet/smartrevaadmin/placements/PlacementsMain.java
@@ -1,10 +1,13 @@
 package com.samkeet.smartrevaadmin.placements;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.samkeet.smartrevaadmin.R;
+import com.samkeet.smartrevaadmin.alumni.*;
+import com.samkeet.smartrevaadmin.alumni.AddEventActivity;
 import com.samkeet.smartrevaadmin.fees.ViewFeeActivity;
 
 public class PlacementsMain extends AppCompatActivity {
@@ -16,4 +19,21 @@ public class PlacementsMain extends AppCompatActivity {
     }
 
     public void BackButton (View v){finish();}
+
+    public void AddEvents (View v){
+        Intent intent =new Intent(getApplicationContext(), AddEventActivity.class);
+        startActivity(intent);
+    }
+    public void ManageEvents (View v){
+        Intent intent = new Intent(getApplicationContext(),ManageEventActivity.class);
+        startActivity(intent);
+    }
+    public void ViewAcademicDetails (View v){
+        Intent intent = new Intent(getApplicationContext(),ViewAcademicDetailsActivity.class);
+        startActivity(intent);
+    }
+    public void SendNotification (View v){
+        Intent intent = new Intent(getApplicationContext(),SendNotificationActivity.class);
+        startActivity(intent);
+    }
 }
