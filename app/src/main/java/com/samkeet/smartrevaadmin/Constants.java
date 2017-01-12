f828b3aa65b54c6c205bcb37be8bb09f680ea9b3
vaibhavkri <vaibhavkrishna.bhosle@gmail.com>
2017-01-12 10:29:11 +0530

    stable commit 12-01

---
+++ app/src/main/java/com/samkeet/smartrevaadmin/Constants.java
@@ -0,0 +1,17 @@
+package com.samkeet.smartrevaadmin;
+
+/**
+ * Created by Blazzing Frost on 1/12/2017.
+ */
+
+public class Constants {
+    public static class URLs{
+        public static String BASE ="http://revacounselling.16mb.com/";
+        public static String ALUMNI_BLOG="ADM_alumni_discussion.php";
+        public static String ALUMNI_EVENTS="ADM_alumni_events.php";
+        public static String ALUMNI_JOB_REFER="ADM_alumni_job_refer.php";
+        public static String ALUMNI_MEMBER_REQUEST="ADM_alumni_member_request.php";
+        public static String ALUMNI_VIEW_PROFILE="ADM_alumni_view_profile.php";
+        public static String ALUMNI_LOGIN_ALUMNI="ADM_login_alumni.php";
+    }
+}
