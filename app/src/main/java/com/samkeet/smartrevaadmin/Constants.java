6bdd8ce8d532a351361823b7bf90e566f590cb79
vaibhavkri <vaibhavkrishna.bhosle@gmail.com>
2017-01-14 14:31:48 +0530

    stable commit

--- app/src/main/java/com/samkeet/smartrevaadmin/Constants.java
+++ app/src/main/java/com/samkeet/smartrevaadmin/Constants.java
@@ -1,10 +1,19 @@
 package com.samkeet.smartrevaadmin;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Blazzing Frost on 1/12/2017.
  */
 
 public class Constants {
+
     public static class URLs{
         public static String BASE ="http://revacounselling.16mb.com/";
         public static String ALUMNI_BLOG="ADM_alumni_discussion.php";
@@ -12,6 +21,95 @@ public class Constants {
         public static String ALUMNI_JOB_REFER="ADM_alumni_job_refer.php";
         public static String ALUMNI_MEMBER_REQUEST="ADM_alumni_member_request.php";
         public static String ALUMNI_VIEW_PROFILE="ADM_alumni_view_profile.php";
-        public static String ALUMNI_LOGIN_ALUMNI="ADM_login_alumni.php";
+        public static String ADMIN_LOGIN="ADM_login.php";
     }
+
+    public static class SharedPreferenceData {
+
+        public static SharedPreferences sharedPreferences=null;
+        public static SharedPreferences.Editor editor=null;
+
+        public static String SHAREDPREFERENCES = "SmartReva";
+        public static String IS_LOGGED_IN = "isloggedin";
+        public static String USER_ID = "user_ID";
+        public static String TOKEN = "token";
+        public static String IS_ALUMNI = "alumni";
+
+        public static void initSharedPreferenceData(SharedPreferences sharedPreferences1) {
+            sharedPreferences = sharedPreferences1;
+            editor = sharedPreferences.edit();
+        }
+
+        public static boolean isSharedPreferenceInited(){
+            if(sharedPreferences!=null)
+                return true;
+            return false;
+        }
+
+        public static String getIsLoggedIn() {
+            return sharedPreferences.getString(IS_LOGGED_IN, "NOT_AVALIBLE");
+        }
+
+        public static String getUserId() {
+            return sharedPreferences.getString(USER_ID, "NOT_AVALIBLE");
+        }
+
+        public static String getTOKEN() {
+            return sharedPreferences.getString(TOKEN, "NOT_AVALIBLE");
+        }
+
+        public static String getIsAlumni(){
+            return sharedPreferences.getString(IS_ALUMNI, "NOT_AVALIBLE");
+        }
+
+        public static void setIsAlumni(String isAlumni){
+            editor.putString(IS_ALUMNI, "yes");
+            editor.apply();
+        }
+
+        public static void setIsLoggedIn(String isLoggedIn) {
+            editor.putString(IS_LOGGED_IN, "yes");
+            editor.apply();
+        }
+
+        public static void setTOKEN(String token) {
+            editor.putString(TOKEN, token);
+            editor.apply();
+        }
+
+        public static void setUserId(String userId) {
+            editor.putString(USER_ID, userId);
+            editor.apply();
+        }
+
+        public static void clearData() {
+            editor.clear();
+            editor.apply();
+
+        }
+    }
+
+    public static class Methods {
+
+        public static boolean networkState(Context context, ConnectivityManager comman) {
+            boolean wifi = comman.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected();
+            boolean data = comman.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected();
+
+            if (wifi || data) {
+                return true;
+            } else {
+                Toast toast = Toast.makeText(context, "No Internet Connnection!!! Try Again", Toast.LENGTH_LONG);
+                toast.show();
+                return false;
+            }
+        }
+
+        public static boolean checkForSpecial(String s) {
+            Pattern p = Pattern.compile("[^A-Za-z0-9 -.@]");
+            Matcher m = p.matcher(s);
+            boolean b = m.find();
+            return  b;
+        }
+    }
+
 }
--- app/src/main/java/com/samkeet/smartrevaadmin/LoginActivity.java
+++ app/src/main/java/com/samkeet/smartrevaadmin/LoginActivity.java
@@ -1,10 +1,27 @@
 package com.samkeet.smartrevaadmin;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import dmax.dialog.SpotsDialog;
 
 import static android.R.attr.button;
 
@@ -12,17 +29,134 @@ import static android.R.attr.button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public EditText usn, password;
+    public Button login_button;
+    public String susn, spassword;
+
+    public SpotsDialog pd;
+    public Context progressDialogContext;
+
+    public String token;
+    public String auth;
+    public boolean authenticationError = true;
+    public String errorMessage = "Data Corrupted";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        progressDialogContext=this;
+
+        Constants.SharedPreferenceData.initSharedPreferenceData(getSharedPreferences(Constants.SharedPreferenceData.SHAREDPREFERENCES, MODE_PRIVATE));
+
+        usn = (EditText) findViewById(R.id.usn);
+        password = (EditText) findViewById(R.id.password);
     }
 
     public void Login(View v){
-        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-        startActivity(intent);
+        susn = usn.getText().toString();
+        spassword = password.getText().toString();
+
+        if (Constants.Methods.networkState(getApplicationContext(), (ConnectivityManager) getSystemService(getApplicationContext().CONNECTIVITY_SERVICE))) {
+            Login login = new Login();
+            login.execute();
+        }
     }
+
+    private class Login extends AsyncTask<Void, Void, Integer> {
+
+        protected void onPreExecute() {
+            pd = new SpotsDialog(progressDialogContext, R.style.CustomPD);
+            pd.setTitle("Loading...");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected Integer doInBackground(Void... params) {
+            try {
+                URL url = new URL(Constants.URLs.BASE + Constants.URLs.ADMIN_LOGIN);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setRequestMethod("POST");
+                Log.d("POST", "DATA ready to sent");
+
+                Uri.Builder _data = new Uri.Builder().appendQueryParameter("UserID", susn).appendQueryParameter("Password", spassword);
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
+                writer.write(_data.build().getEncodedQuery());
+                writer.flush();
+                writer.close();
+                Log.d("POST", "DATA SENT");
+
+                InputStreamReader in = new InputStreamReader(connection.getInputStream());
+
+                StringBuilder jsonResults = new StringBuilder();
+                // Load the results into a StringBuilder
+                int read;
+                char[] buff = new char[1024];
+                while ((read = in.read(buff)) != -1) {
+                    jsonResults.append(buff, 0, read);
+                }
+                connection.disconnect();
+                Log.d("return from server", jsonResults.toString());
+
+                authenticationError = jsonResults.toString().contains("Authentication Error");
+
+                if (authenticationError) {
+                    errorMessage = jsonResults.toString();
+                } else {
+                    // Create a JSON object hierarchy from the results
+                    JSONObject jsonObj = new JSONObject(jsonResults.toString());
+                    String status = jsonObj.getString("status");
+                    if (status.equals("success")) {
+                        token = jsonObj.getString("token");
+                        auth = jsonObj.getString("auth");
+                        authenticationError = false;
+                    } else {
+                        authenticationError = true;
+                        errorMessage = status;
+                    }
+                }
+
+                return 1;
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            return 1;
+        }
+
+        protected void onPostExecute(Integer result) {
+            if (pd != null) {
+                pd.dismiss();
+            }
+            if (authenticationError) {
+                Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_SHORT).show();
+            } else {
+
+                Constants.SharedPreferenceData.setIsLoggedIn(token);
+                Constants.SharedPreferenceData.setTOKEN(token);
+                Constants.SharedPreferenceData.setUserId(susn);
+
+                /*if (Constants.FireBase.token != null) {
+                    UpdateToken updateToken = new UpdateToken();
+                    updateToken.execute();
+                }else {
+                    FirebaseInstanceId.getInstance().getToken();
+                    Intent intent = new Intent(getApplicationContext(),AlumniMainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }*/
+
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+
+            }
+
+        }
+    }
+
 
 
 }
