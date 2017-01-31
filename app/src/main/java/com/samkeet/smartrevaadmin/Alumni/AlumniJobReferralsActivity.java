286e4c849eb8ec13dd9c93cbea3bb67c33cfbcec
vaibhavkrishna <vaibhavkrishna.bhosle@gmail.com>
2017-01-31 18:13:55 +0530

    referrals updated, with Alumni bug fix

---
+++ app/src/main/java/com/samkeet/smartrevaadmin/Alumni/AlumniJobReferralsActivity.java
@@ -0,0 +1,30 @@
+package com.samkeet.smartrevaadmin.Alumni;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.samkeet.smartrevaadmin.R;
+
+public class AlumniJobReferralsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alumni_job_referrals);
+    }
+    public void JobReferalsAcc (View v){
+        Intent intent = new Intent(getApplicationContext(),AlumniJobReferralAcceptedActivity.class);
+        startActivity(intent);
+    }
+    public void JobReferalsRej (View v){
+        Intent intent = new Intent(getApplicationContext(),AlumniJobReferralRejectedActivity.class);
+        startActivity(intent);
+    }
+    public void JobReferalsPend (View v){
+        Intent intent = new Intent(getApplicationContext(),AlumniJobReferalActivity.class);
+        startActivity(intent);
+    }
+    public void BackButton(View v){finish();}
+}
